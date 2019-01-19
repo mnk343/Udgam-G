21bb7d27dd7b8998bbaf79d7059644d04898718e
mnk343 <mayank343@gmail.com>
2019-01-19 16:36:04 +0530

    2 m

--- clinic/appointments/views.py
+++ clinic/appointments/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+@login_required
+def dashboard(request):
+    if request.user.person=='patient':
+        patient = models.Patient.objects.filter(user=request.user)
+        if not patient :
+            form = forms.UpdatePatientDetail()
+            if request.method=='POST' :
+                form = forms.UpdatePatientDetail(request.POST)
+                if form.is_valid():
+                    detail = form.save(commit=False)
+                    detail.user = request.user
+                    detail.save()
+                    #we are getting an error
+                return redirect('appointments:dashboard')
+            else :
+                return render(request,'appointments/PatientDetail.html', {'form':form , 'patient':request.user.username} )
+        else:
+            patient = models.Patient.objects.get(user=request.user)
+            forms = models.ApplyLeave.objects.filter(patient=patient)
+            return render(request,'appointments/dashboard.html',{'user':request.user , 'patient':patient, 'forms':forms})
+
+    elif request.user.person=='doctor':
+        doctor = models.Doctor.objects.filter(user=request.user)
+        if not doctor :
+            form = forms.UpdateDoctorDetail()
+            if request.method=='POST' :
+                form = forms.UpdateDoctorDetail(request.POST)
+                if form.is_valid():
+                    detail = form.save(commit=False)
+                    detail.user = request.user
+                    detail.save()
+                    #we are getting an error
+                return redirect('appointments:dashboard')
+            else :
+                return render(request,'appointments/doctor_update_detail.html', {'form':form , 'doctor':request.user.username} )
+        else:
+            doctor = models.Doctor.objects.get(user=request.user)
+            return render(request,'leave_portal/authorized_dashboard.html',{'user':request.user , 'doctor':doctor})
