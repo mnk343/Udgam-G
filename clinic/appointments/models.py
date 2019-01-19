f0a2b2af919d83f9987b1d6a25a526a74ab47c43
mnk343 <mayank343@gmail.com>
2019-01-19 16:03:42 +0530

    mayank 1st

--- clinic/appointments/models.py
+++ clinic/appointments/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from users.models import CustomUser
+from multiselectfield import MultiSelectField
 from django.db import models
 
 sex = (
@@ -5,18 +8,34 @@ sex = (
         ('female','female'),
      )
 
+days = (
+        ('monday' , 'monday') ,
+        ('tuesday' , 'tuesday') ,
+        ('wednesday' , 'wednesday') ,
+        ('thursday' , 'thursday') ,
+        ('friday' , 'friday') ,
+        ('saturday' , 'saturday') ,
+        ('sunday' , 'sunday') ,
+)
+
 class Patient (models.Model):
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE , null=True)
+
     name = models.CharField(max_length = 200 , blank = False)
     age = models.IntegerField(default = 18)
     sex = models.CharField(max_length = 100 , choices = sex )
 
 class Doctor(models.Model):
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+
+
     name = models.CharField(max_length = 200 , blank = False)
-    # slot = models.()
 
 
 class Slot(models.Model):
-    # day = models.CharField(choices = days)
+    day = models.CharField(max_length = 100 ,choices = days , default='monday')
     time = models.IntegerField( blank = False)
     availability = models.IntegerField(default = 0 , blank = False)
-    # doctor = models.()
+    doctor = models.ForeignKey(Doctor , on_delete=models.CASCADE , null=True)
